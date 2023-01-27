8d6f9e64de7d09300bf408fc54fc41152bf8fbb0
CodeThinkers' Software Application Academy <61565997+Ahmed-Dauda@users.noreply.github.com>
2023-01-27 21:08:34 +0100

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>testing api</h1>
+        <h1>testing api2</h1>
          <Home/>
         <HomePage />
       </div>
